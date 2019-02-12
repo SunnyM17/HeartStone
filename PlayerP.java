0c71d7607ad9b26e268b6c8baaa2688718298395
basselhasan <47043095+basselhasan@users.noreply.github.com>
2019-02-12 16:02:10 -0700

    Add files via upload

---
+++ PlayerP.java
@@ -0,0 +1,42 @@
+public class Player
+{
+    //instance variables
+    private String PlayerName;
+    private int maxHealth;
+    private int maxEnergy;
+    private int remainingHealth;
+    private int remainingEnergy;
+    private int block;
+    private Deck playerDeck;
+
+    startingDeck = new Deck();
+    for (int i = 1; i == 5; x++);
+      startingDeck.add("Slash", 1, 6, 0, 0);
+      startingDeck.add("Guard", 1, 0, 5, 0);
+
+    public Player(String PlayerName)
+    {
+        this.PlayerName = PlayerName;
+        this.maxHealth = 80;
+        this.remainingHealth = 80;
+        this.maxEnergy = 3;
+        this.remainingEnergy = 3;
+        this.block = 0;
+        this.playerDeck = startingDeck;
+    }
+    void altHealth(int amount)
+    {
+        remainingHealth -= amount;
+
+    }
+
+    void altEnergy(int amount)
+    {
+        remainingEnergy -= amount;
+    }
+
+    void altBlock(int amount)
+    {
+        block -= amount;
+    }
+}

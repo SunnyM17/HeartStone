35f8a327f88efeae2f8852f88efd19a7116d6703
SunnyM17 <47331378+SunnyM17@users.noreply.github.com>
2019-02-12 14:36:50 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,38 @@
+import java.util.array
+
+public class Card {
+
+  //Instance Variables
+  private String cardName;
+  private int energyCost;
+  private int damageValue;
+  private int blockValue;
+  private int drawValue;
+
+  //Constructors
+  public Card (String cardName, int energyCost, int damageValue, int blockValue, int drawValue) {
+    this.cardName = cardName;
+    this.energyCost = energyCost;
+    this.damageValue = damageValue;
+    this.blockValue = blockValue;
+    this.drawValue = drawValue;
+  }
+
+  //Acesor Method
+  public String getCardDescription() {
+    String cardDescription = (cardName + ": "+"Energy Cost: " + energyCost);
+    if (damageValue > 0) {
+      cardDescription += (", Deals "+damageValue+" damage");
+    }
+    if (blockValue > 0) {
+      cardDescription += (", Blocks "+blockValue+" damage");
+    }
+    if (drawValue == 1) {
+      cardDescription += (", Draws "+drawValue+" card");
+    } else if (drawValue > 0) {
+      cardDescription += (", Draws "+drawValue+" cards");
+    }
+    cardDescription += ".";
+    return cardDescription;
+  }
+}

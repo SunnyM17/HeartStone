4d925406ca9fabaf5765693773988488780fccc8
thehahaman <47429196+thehahaman@users.noreply.github.com>
2019-02-12 20:50:07 -0700

    Update PlayerP.java

--- Demo 1 Files/PlayerP.java
+++ Demo 1 Files/PlayerP.java
@@ -1,4 +1,4 @@
-public class Player
+public class PlayerP
 {
     //instance variables
     private String PlayerName;
@@ -8,15 +8,8 @@ public class Player
     private int remainingEnergy;
     private int block;
     private Deck playerDeck;
-
-    startingDeck = new Deck();
-    for (int i = 1; i <= 5; x++)
-    {
-      startingDeck.deck.add(new Card("Slash", 1, 6, 0, 0));
-      startingDeck.deck.add(new Card("Guard", 1, 0, 5, 0));
-    }
         
-    public Player(String PlayerName)
+    public PlayerP(String PlayerName)
     {
         this.PlayerName = PlayerName;
         this.maxHealth = 80;
@@ -24,6 +17,12 @@ public class Player
         this.maxEnergy = 3;
         this.remainingEnergy = 3;
         this.block = 0;
+        startingDeck = new Deck();
+        for (int i = 1; i <= 5; i++)
+        {
+          startingDeck.deck.add(new Card("Slash", 1, 6, 0, 0));
+          startingDeck.deck.add(new Card("Guard", 1, 0, 5, 0));
+        }
         this.playerDeck = startingDeck;
     }
     void altHealth(int amount)

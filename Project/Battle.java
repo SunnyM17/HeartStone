7332804c633dfc3e6563bdebec4d730877e85f6b
basselhasan <47043095+basselhasan@users.noreply.github.com>
2019-03-12 12:00:55 -0600

    Update Battle.java

--- Project/Battle.java
+++ Project/Battle.java
@@ -10,6 +10,7 @@ public class Battle
       int noCard = 0;
       int blockTurnP = 0;
       int blockTurnE = 1;
+        
       //Ongoing Decks that will be altered as battle goes on
       Deck ongDeckP =  new Deck();
       Deck ongDeckE = new Deck();

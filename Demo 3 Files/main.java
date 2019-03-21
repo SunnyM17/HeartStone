56dbff9158e7c722cada55ff92b127ac8535382b
SunnyM17 <47331378+SunnyM17@users.noreply.github.com>
2019-03-20 21:18:39 -0600

    Corrected while loop in main

--- Demo 3 Files/main.java
+++ Demo 3 Files/main.java
@@ -61,12 +61,11 @@ public class main{
         //Initializing the player
         Player Player1 = new Player(playerName);
 
-        int enemyHealth = 80;
-        int playerHealth = 80;
+        
         //Player undergoes one battle against each enemy
         
 
-        while(playerHealth > 0 || enemyHealth > 0)
+        while(Player1.getRemainingHealth() > 0 && GiantRat.getRemainingHealth() > 0)
         {
             BATTLE1 battle = new BATTLE1(Player1, GiantRat);
             battle.enemyTurn(Player1, GiantRat);
@@ -100,8 +99,6 @@ public class main{
             System.out.println(Player1.getPlayerInformation());
             System.out.println(GiantRat.getEnemyInformation());
             System.out.println();
-            playerHealth = Player1.getRemainingHealth();
-            enemyHealth = GiantRat.getRemainingHealth();
             System.out.println("---------------------LOOP END------------------");
             battle.swichTurn(false, true);
             

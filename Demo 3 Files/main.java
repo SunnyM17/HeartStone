b4db0bbe7e01782f3ee1f775581dcbc9a931e096
thehahaman <47429196+thehahaman@users.noreply.github.com>
2019-03-26 22:47:28 -0600

    Update main.java

--- Demo 3 Files/main.java
+++ Demo 3 Files/main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
 
 
 /**
 * <h1>Main!</h1>
 * The Main program brings together a functioning and engaging
-* game by utilizing the methods and objects of battle, player,
+* game by utilising the methods and objects of battle, player,
 * enemy, card and deck classes.
 */
 public class main{
@@ -13,57 +15,100 @@ public class main{
 
     public static void startBattle(String playerName, Player Player1, Enemy enemy)
     {
+    	BATTLE1 battle = new BATTLE1(Player1, enemy);
+    	boolean b = true;
         Scanner keyboard = new Scanner(System.in);
         while (Player1.getRemainingHealth() > 0 && enemy.getRemainingHealth() > 0)
+        {		
+            while(b)
             {
-            BATTLE1 battle = new BATTLE1(Player1, enemy);
-            while(battle.enemyTurn(Player1, enemy) == true)
-            {
-                battle.enemyTurn(Player1, enemy);
-                
+            	b = battle.enemyTurn(Player1, enemy);                
+	            System.out.println();
+	            System.out.println(Player1.getPlayerInformation());
+	            System.out.println(enemy.getEnemyInformation());
+	            System.out.println();
+	            System.out.println();
+	            System.out.println();	            
             }
             
-            System.out.println("Enemy played " + battle.initializeEHand(enemy).getCard(battle.getenemyCardIndex()).showCardDescription());
-            System.out.println();
-            System.out.println(Player1.getPlayerInformation());
-            System.out.println(enemy.getEnemyInformation());
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            //battle.swichTurn(true, false);
-            System.out.print("You have the following cards in your hand: ");
-            System.out.println("Enter the name of the card to play it. |");
-            for(int i = 0; i < battle.initializePHand(Player1).getDeckList().size(); i++)
+            battle.refreshOngDeckE();
+            battle.initializeEHand(enemy);
+            
+            if(Player1.getRemainingHealth() > 0)
             {
-                System.out.print(battle.initializePHand(Player1).getCard(i).getCardName() + " | ");
-            }
-            System.out.println();
-            String card = keyboard.nextLine().toUpperCase();
-            for(int r = 1; r <= battle.initializePHand(Player1).getDeckList().size(); r++)
-            {
-                if(card.equals("" + r))
-                {
-                    card = battle.initializePHand(Player1).getCard(r-1).getCardName();
-                    break;
-                }
+            	b = true;
             }
             
-            System.out.println(playerName + " played " + battle.initializePHand(Player1).getCard(card).showCardDescription());
-            battle.playerTurn(card, Player1, enemy);
-            System.out.println();
-            System.out.println(Player1.getPlayerInformation());
-            System.out.println(enemy.getEnemyInformation());
-            System.out.println();
-           
-            //battle.swichTurn(false, true);
+            if (battle.getBlockPlayer() == 1)
+        	{
+        		Player1.setBlock(0);
+        		battle.setBlockPlayer(0);
+        	}
+            
+            if (enemy.getBlock() > 0)
+        	{
+        		battle.setBlockEnemy(1);
+        	}
+            
+            while(b)
+            {
+            	
+	            //battle.swichTurn(true, false);
+	            System.out.print("You have the following cards in your hand: ");
+	            System.out.print("Enter the name of the card to play it. \n|");
+	            for(int i = 0; i < battle.getPlayerHand().getDeckList().size(); i++)
+	            {
+	                System.out.print(battle.getPlayerHand().getCard(i).getCardName() + " | ");
+	            }
+	            System.out.println();
+	            String card = keyboard.nextLine().toUpperCase();
+	            int cardToPlay = 0;
+	            for(int r = 1; r <= battle.getPlayerHand().getDeckList().size(); r++)
+	            {
+	                if( card.equals("" + r) || card.equals(battle.getPlayerHand().getCard(r-1).getCardName()) )
+	                {
+	                    cardToPlay = (r-1);
+	                    
+	                    break;
+	                }
+	            }
+	            
+	            System.out.println(playerName + " played " + battle.getPlayerHand().getCard(cardToPlay).showCardDescription());
+	            b = battle.playerTurn(cardToPlay, Player1, enemy);
+	            
+	            System.out.println();
+	            System.out.println(Player1.getPlayerInformation());
+	            System.out.println(enemy.getEnemyInformation());
+	            System.out.println();
+            }
+            
+            battle.refreshOngDeckP();
+            battle.initializePHand(Player1);
+            
+            if (battle.getBlockEnemy() == 1)
+        	{
+        		enemy.setBlock(0);
+        		battle.setBlockEnemy(0);
+        	}
+        	
+            if (Player1.getBlock() > 0)
+        	{
+        		battle.setBlockPlayer(1);
+        	}
+            
+            if(enemy.getRemainingHealth() > 0)
+            {
+            	b = true;
+            }
+            
             if (enemy.getRemainingHealth() <= 0)
             {
                 Random rand1 = new Random();
-                int randomCard1 = rand1.nextInt(battle.initializeEHand(enemy).getDeckList().size());
+                int randomCard1 = rand1.nextInt(enemy.getDeck().getDeckList().size());
                 Random rand2 = new Random();
-                int randomCard2 = rand2.nextInt(battle.initializeEHand(enemy).getDeckList().size());
+                int randomCard2 = rand2.nextInt(enemy.getDeck().getDeckList().size());
                 Random rand3 = new Random();
-                int randomCard3 = rand3.nextInt(battle.initializeEHand(enemy).getDeckList().size());
+                int randomCard3 = rand3.nextInt(enemy.getDeck().getDeckList().size());
 
                 System.out.println(enemy.getDeck().getCard(randomCard1).showCardDescription());
                 System.out.println(enemy.getDeck().getCard(randomCard2).showCardDescription());
@@ -100,20 +145,18 @@ public class main{
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
          //The master deck is a collection of all the cards in the game
         Deck masterDeck = new Deck();
-        masterDeck.addCard(new Card("SLASH", 1, 6, 0),1);
-        masterDeck.addCard(new Card("GUARD", 1, 0, 5),1);
-        masterDeck.addCard(new Card("CLAW", 1, 3, 3),1);
-        masterDeck.addCard(new Card("FIRE BALL", 2, 13, 0),1);
-        masterDeck.addCard(new Card("ICE BLOCK", 2, 0, 11),1);
-        masterDeck.addCard(new Card("HEALING WAVE", 1, -5, 0),1);
-        masterDeck.addCard(new Card("STOMP", 2, 5, 5),1);
-        masterDeck.addCard(new Card("CHARGE", 3, 28, 0),1);
-        masterDeck.addCard(new Card("THICK HIDE", 0, 0, 4),1);
-        masterDeck.addCard(new Card("pass", 0 ,0 ,0),1);
-
+        Scanner input = new Scanner(new File("cardList.txt"));
+        while(input.hasNext())
+        {
+        	
+        	masterDeck.addCard(new Card(input.next(), Integer.parseInt(input.next()), Integer.parseInt(input.next()), Integer.parseInt(input.next())) , 1);
+        }
+        
+    	input.close();
+    		
         //Enemy Deck(GiantRat)
         Deck GiantRatDeck = new Deck();
         GiantRatDeck.addCard(masterDeck.getCard("SLASH"),3);
@@ -150,7 +193,7 @@ public class main{
 
         //Initializing the player
         Player Player1 = new Player(playerName);
-
+        
         while (Player1.getRemainingHealth() > 0)
         {
             startBattle(playerName,Player1, GiantRat);

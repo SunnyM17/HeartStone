73c0bdae5aba599e57f7bfce1d0a91e8d077f50c
SunnyM17 <47331378+SunnyM17@users.noreply.github.com>
2019-04-11 20:36:07 -0600

    Folder containing files with no packages

---
+++ Final Submission- no packages/Tests/DeckTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import java.util.ArrayList;
+import org.junit.Test;
+
+// ---------- TEAM 08 --------
+// --Guransh Mangat, Bassel Hassan, George Abouseta, Yihan Wei, Liam Miller -----
+
+public class DeckTest
+{
+      @Test
+      public void test_addCard_CheckDeckSize()
+      {
+          Deck d = new Deck();
+          Card c1 = new Card();
+          Card c2 = new Card();
+          d.addCard(c1,2);
+          d.addCard(c2,1);
+          ArrayList<Card> deck = d.getDeckList();
+          assertEquals("The Deck size expected was", 3 , deck.size(),0.000001);
+    }
+}
